7dafda7ee91ab11487c22d074824098d2d71e394
Simon Lock <simon.lock@bristol.ac.uk>
2022-02-26 16:58:02 +0000

    Added system folders

--- scripts/quick-register.py
+++ scripts/quick-register.py
@@ -3,13 +3,14 @@ import utilities
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-if len(sys.argv) < 2: print("Usage: python3 " + sys.argv[0] + " <username list>")
+if len(sys.argv) < 2: print("Usage: python3 " + sys.argv[0] + " <student number list>")
 else:
     notes = input("Please enter some note text for all students: ")
     driver = utilities.initialiseDriver()
     # utilities.hideBrowser(driver)
 
     for i in range(1,len(sys.argv)):
-        nextUsername = sys.argv[i]
-        studentNumber = utilities.getStudentNumber(driver, nextUsername)
-        utilities.logAttendanceForStudent(driver, studentNumber, notes)
+        nextStudentNumber = sys.argv[i]
+        # Just use the last 7 characters - in case the ident starts with a nickname
+        nextStudentNumber = nextStudentNumber[-7:]
+        utilities.logAttendanceForStudent(driver, nextStudentNumber, notes)
